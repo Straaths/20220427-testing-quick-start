37419013466c7f54515876bdefdcddd66cdf2007
Jakub Mintal <Straaths@gmail.com>
2022-04-27 19:15:11 +0200

    Add basic tests
    1. Added "test" and "test:watch" scripts for running jest test framework.
    2. Added "sanity test" for 'forms-errors' and 'form-validatios'.

---
+++ src/homeworks/hw-day-1/src/validators/form-errors.spec.js
@@ -0,0 +1,6 @@
+it(
+    "should do sanity test of form-errors",
+    () => {
+        expect(2+3).toBe(5);
+    }
+);
---
+++ src/homeworks/hw-day-1/src/validators/form-validators.spec.js
@@ -0,0 +1,6 @@
+it(
+    "should do sanity test of form-validatiors",
+    () => {
+        expect(1+2).toBe(3);
+    }
+);
