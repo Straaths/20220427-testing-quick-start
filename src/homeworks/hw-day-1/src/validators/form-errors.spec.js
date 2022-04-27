c3b65986793130313c49e7ffbb4452f8defa2807
Jakub Mintal <Straaths@gmail.com>
2022-04-27 19:48:53 +0200

    Add test suits for form-validators and form-errors
    Added basic test suits which not really do much.

--- src/homeworks/hw-day-1/src/validators/form-errors.spec.js
+++ src/homeworks/hw-day-1/src/validators/form-errors.spec.js
@@ -4,3 +4,23 @@ it(
         expect(2+3).toBe(5);
     }
 );
+
+import { 
+    getValidationErrorMessages,  
+    hasAnyErrors
+} from "./form-errors";
+
+describe(
+    "module form-errors",
+    () => {
+        it(
+            "should test required fields",
+            () => {
+                expect.hasAssertions();
+
+                const result = getValidationErrorMessages({});
+                expect(result).toEqual([]);
+            }
+        );
+    }
+);
--- src/homeworks/hw-day-1/src/validators/form-validators.spec.js
+++ src/homeworks/hw-day-1/src/validators/form-validators.spec.js
@@ -4,3 +4,31 @@ it(
         expect(1+2).toBe(3);
     }
 );
+
+import { 
+    validateRequired,  
+    validateMinLength,
+    validateEmail,
+    validatePassword
+} from "./form-validators";
+
+describe(
+    "module form-validatior",
+    () => {
+        it(
+            "should test required fields",
+            () => {
+                expect.hasAssertions();
+
+                const resultForEmpty = validateRequired();
+                expect(resultForEmpty).toEqual({required: "This field is required"});
+
+                const resultForString = validateRequired('A string.');
+                expect(resultForString.required).toBe('');
+
+                const resultForZero = validateRequired(0);
+                expect(resultForZero.required).toBe('This field is required');
+            }
+        );
+    }
+);
