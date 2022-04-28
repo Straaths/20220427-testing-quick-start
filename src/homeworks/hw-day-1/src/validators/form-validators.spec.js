005754656e0010717d63526999ed8658a7182b7e
Jakub Mintal <Straaths@gmail.com>
2022-04-28 08:57:23 +0200

    Add tests for all functions from form-validators
    added test for:
    - validateMinLength
    - validateEmail
    - validatePassword

--- src/homeworks/hw-day-1/src/validators/form-validators.spec.js
+++ src/homeworks/hw-day-1/src/validators/form-validators.spec.js
@@ -13,7 +13,7 @@ import {
 } from "./form-validators";
 
 describe(
-    "module form-validatior",
+    "module form-validator",
     () => {
         it(
             "should test required fields",
@@ -28,6 +28,44 @@ describe(
 
                 const resultForZero = validateRequired(0);
                 expect(resultForZero.required).toBe('This field is required');
+            }
+        );
+
+        it(
+            "should test validateMinLength",
+            () => {
+                let result = validateMinLength('', 3);
+                expect(result).toEqual({"minLength": "Field must have at least 3 chars"});
+            }
+        );
+
+        it(
+            "should test if email is valid",
+            () => {
+                let resultError = validateEmail('abc', 3);
+                expect(resultError).toEqual({"email": "Invalid e-mail address"});
+
+                let resultOk = validateEmail('john@doe.com', 3);
+                expect(resultOk).toEqual({"email": ""});
+            }
+        );
+
+        it(
+            "should test validatePassword",
+            () => {
+                let resultError = validatePassword('weakPassword');
+                expect(resultError).toEqual( 
+                    {
+                        password: "Password: must have special sign (#!$), must have a digit"
+                    } 
+                );
+
+                let resultOk = validatePassword('str0nGPa55word!');
+                expect(resultOk).toEqual( 
+                    {
+                        password: ""
+                    } 
+                );
             }
         );
     }
